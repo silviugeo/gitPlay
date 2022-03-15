0248dd8accebfbd39466fe02b317f60da08b4c6e
Silviu Ungureanu <silviugeorge.ungureanu@endava.com>
2022-03-15 13:23:37 +0200

    JIRA-2 - introduced first bug

--- math.c
+++ math.c
@@ -5,3 +5,8 @@ int add (int a, int b) {
 int multiply(int a, int b) {
 	return a*b;
 }
+
+/*This is a comment*/
+int division(int a, int b) {
+	return a*b;
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+This is a math library
+Please use it accordingly
+
+New line in readme file
